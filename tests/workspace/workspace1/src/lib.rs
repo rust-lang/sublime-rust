ae61e332ea5939a7653cd0372e81346c47612420
Eric Huss <eric@huss.org>
2017-06-08 12:42:23 -0700

    Update tests for new messages in rust 1.18.

--- tests/workspace/workspace1/src/lib.rs
+++ tests/workspace/workspace1/src/lib.rs
@@ -1,8 +1,11 @@
 mod anothermod;
 
 /*BEGIN*/struct S {
+//       ^^^^^^^^ERR(>=1.18.0) recursive type has infinite size
+//       ^^^^^^^^ERR(>=1.18.0) recursive type `S` has infinite size
+//       ^^^^^^^^HELP(>=1.18.0) insert indirection
     recursive: S
 }/*END*/
-// ~ERR recursive type has infinite size
-// ~ERR recursive type
-// ~HELP insert indirection
+// ~ERR(<1.18.0) recursive type has infinite size
+// ~ERR(<1.18.0) recursive type `S` has infinite size
+// ~HELP(<1.18.0) insert indirection
--- tests/workspace/workspace2/src/lib.rs
+++ tests/workspace/workspace2/src/lib.rs
@@ -9,4 +9,5 @@ pub fn f() {
 //                  ^ERR mismatched types
 //                  ^NOTE expected type `&Trait`
 //                  ^NOTE(<1.16.0) found type
+//                  ^HELP(>=1.18.0) try with `&x`
 }
