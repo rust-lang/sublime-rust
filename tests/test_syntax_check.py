6b3be738cbabe461905d6fbb2720570fc13c4b71
Eric Huss <eric@huss.org>
2017-08-21 14:46:21 -0700

    Add test for macro expansion with attribute.

--- tests/test_syntax_check.py
+++ tests/test_syntax_check.py
@@ -103,6 +103,7 @@ class TestSyntaxCheck(TestBase):
             ('error-tests/tests/macro-expansion-inside-1.rs', 'error-tests/tests/macro_expansion_inside_mod1.rs'),
             ('error-tests/tests/macro-expansion-inside-2.rs', 'error-tests/tests/macro_expansion_inside_mod2.rs'),
             ('error-tests/tests/error_across_mod.rs', 'error-tests/tests/error_across_mod_f.rs'),
+            'error-tests/tests/derive-error.rs',
             # Workspace tests.
             'workspace/workspace1/src/lib.rs',
             'workspace/workspace1/src/anothermod/mod.rs',
