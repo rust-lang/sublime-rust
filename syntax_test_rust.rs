01181e27ff52903176834fc02a3fd255e84b52d1
Eric Huss <eric@huss.org>
2018-03-05 13:39:58 -0800

    Add union to syntax.

--- syntax_test_rust.rs
+++ syntax_test_rust.rs
@@ -1160,3 +1160,34 @@ fn main() {
     }.collect::<Vec<_>>();
     println!("{:?}", l);
 }
+
+union Union {
+//^^^ meta.union storage.type.union
+//^^^^^^^^^^^ meta.union
+//    ^^^^^ entity.name.union
+//          ^ meta.block punctuation.definition.block.begin
+    f: u32,
+//  ^ meta.union meta.block variable.other.member
+//   ^ meta.union meta.block punctuation.separator
+//     ^^^ meta.union meta.block storage.type
+}
+// <- meta.union meta.block punctuation.definition.block.end
+
+pub union Foo<'a, Y: Baz>
+// <- storage.modifier
+//^^^^^^^^^^^^^^^^^^^^^^^ meta.union
+//  ^^^^^ meta.union storage.type.union
+//        ^^^ meta.union meta.generic entity.name.union
+//           ^ meta.union meta.generic meta.generic punctuation.definition.generic.begin
+//            ^^ meta.union meta.generic meta.generic storage.modifier.lifetime
+    where X: Whatever,
+//  ^^^^^ meta.union meta.where keyword.other
+//        ^ meta.union meta.where
+//         ^ meta.union meta.where punctuation.separator
+//           ^^^^^^^^^^ meta.union meta.where
+{
+// <- meta.union meta.block punctuation.definition.block.begin
+    f: SomeType, // Comment beside a field
+//               ^^^^^^^^^^^^^^^^^^^^^^^^^^ meta.union meta.block comment.line.double-slash
+}
+// <- meta.union meta.block punctuation.definition.block.end
